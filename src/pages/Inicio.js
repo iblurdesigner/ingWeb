cb3e439537ef8c244635392c04b22bf87caf5b0d
iblurdesigner <nevomen@gmail.com>
2020-01-12 14:38:12 -0500

    optimizacion 02 de imagenes

--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
 import { Link } from 'react-router-dom'
-import inicioImg from '../images/inicio.png'
+import inicioImg from '../images/inicio2.jpg'
 
 class Inicio extends Component {
     render() {
